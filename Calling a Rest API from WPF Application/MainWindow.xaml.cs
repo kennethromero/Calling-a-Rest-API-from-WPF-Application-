7b24185db7a82652a20c8cf3da3e96b0c59ce74d
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-11-03 17:01:56 +0400

    created calling a rest API from WPF Application

--- Calling a Rest API from WPF Application/MainWindow.xaml.cs
+++ Calling a Rest API from WPF Application/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Newtonsoft.Json;
+using RestSharp;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,27 @@ namespace Calling_a_Rest_API_from_WPF_Application
         public MainWindow()
         {
             InitializeComponent();
+
+            var client = new RestClient("https://jsonplaceholder.typicode.com/posts/17");
+
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var content = response.Content;
+
+            var data = JsonConvert.DeserializeObject<Post>(content);
+
+            lblUserId.Content = "UserId:" + data.userId;
+            lblId.Content = "Id:" + data.id;
+            lblBody.Content = "Body:" + data.body;
+            lblTitle.Content = "Title:" + data.title;
+        }
+
+
+        private void btnClickme_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
---
+++ Calling a Rest API from WPF Application/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_a_Rest_API_from_WPF_Application
+{
+    public class Post
+    {
+        public string userId { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+
+
+    }
+}
