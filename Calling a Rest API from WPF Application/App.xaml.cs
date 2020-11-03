ef9439d6afe40e941277fa1c824518859f2f56c0
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-11-03 15:30:18 +0400

    created  a file for Calling a Rest API from WPF Application

---
+++ Calling a Rest API from WPF Application/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calling_a_Rest_API_from_WPF_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
